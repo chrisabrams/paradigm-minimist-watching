987cb27f3cb0ef320828e9cd15bbeca7659538ee
Chris Abrams <mail@chrisabrams.com>
2015-02-08 17:08:38 -0500

    fall back to ES5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var minimist = require('minimist'),
     argv     = minimist(process.argv.slice(2))
 
-module.exports = () => {
+module.exports = function() {
 
   if(argv._.indexOf('w') > -1 || argv._.indexOf('watch') > -1 || argv._.indexOf('watchify') > -1) {
     return true
